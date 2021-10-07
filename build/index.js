8adfb6e1ee91328110daaa4a78e989443ce67032
Marius <mail@mwegener.com>
2021-10-07 16:19:11 -0300

    .

--- build/index.js
+++ build/index.js
@@ -1,6 +1,9 @@
-import { withInfoPlist } from "@expo/config-plugins";
-export const withImageCropPicker = (config) => {
-    return withInfoPlist(config, (config) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.withImageCropPicker = void 0;
+const config_plugins_1 = require("@expo/config-plugins");
+const withImageCropPicker = (config) => {
+    return (0, config_plugins_1.withInfoPlist)(config, (config) => {
         config.modResults.NSPhotoLibraryUsageDescription =
             "Diese App benötigt Zugriff auf deine Fotos";
         config.modResults.NSCameraUsageDescription =
@@ -10,5 +13,5 @@ export const withImageCropPicker = (config) => {
         return config;
     });
 };
-export default withImageCropPicker;
-//# sourceMappingURL=index.js.map
+exports.withImageCropPicker = withImageCropPicker;
+exports.default = exports.withImageCropPicker;
